437dfaf589ca577e5d06fcc5e30580e4ecf272b9
槑从垚 <1806545042@qq.com>
2022-11-26 15:59:21 +0800

    声音方法更新

---
+++ src/Swing/TestPanel.java
@@ -0,0 +1,188 @@
+/*
+ * Created by JFormDesigner on Wed Nov 23 16:29:31 HKT 2022
+ */
+
+package Swing;
+
+import Util.SoundUtil;
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+/**
+ * @author B20040628
+ */
+public class TestPanel extends JFrame {
+    public TestPanel() {
+        initComponents();
+    }
+
+    private void button1MouseClicked(MouseEvent e) {
+        Player player = new Player();
+        Pattern pattern1 = new Pattern("I106 C5 ");
+        Pattern song = new Pattern();
+        song.add(pattern1);
+        player.play(song);
+    }
+
+    private void button2MouseClicked(MouseEvent e) {
+        Player player = new Player();
+        Pattern pattern1 = new Pattern("I106 D5 ");
+        Pattern song = new Pattern();
+        song.add(pattern1);
+        player.play(song);
+    }
+
+    private void button3MouseClicked(MouseEvent e) {
+        Player player = new Player();
+        Pattern pattern1 = new Pattern("I106 E5 ");
+        Pattern song = new Pattern();
+        song.add(pattern1);
+        player.play(song);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        button1 = new JButton();
+        button2 = new JButton();
+        button3 = new JButton();
+        button4 = new JButton();
+        button5 = new JButton();
+        button6 = new JButton();
+        button7 = new JButton();
+        button8 = new JButton();
+        buttonBar = new JPanel();
+        okButton = new JButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPane ========
+        {
+            dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(new GridBagLayout());
+                ((GridBagLayout)contentPanel.getLayout()).columnWidths = new int[] {55, 55, 55, 55, 55, 55, 55, 50, 0};
+                ((GridBagLayout)contentPanel.getLayout()).rowHeights = new int[] {55, 0, 0, 0};
+                ((GridBagLayout)contentPanel.getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+                ((GridBagLayout)contentPanel.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+
+                //---- button1 ----
+                button1.setText("1");
+                button1.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        button1MouseClicked(e);
+                    }
+                });
+                contentPanel.add(button1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+
+                //---- button2 ----
+                button2.setText("2");
+                button2.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        button2MouseClicked(e);
+                    }
+                });
+                contentPanel.add(button2, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+
+                //---- button3 ----
+                button3.setText("3");
+                button3.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        button3MouseClicked(e);
+                    }
+                });
+                contentPanel.add(button3, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+
+                //---- button4 ----
+                button4.setText("4");
+                contentPanel.add(button4, new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+
+                //---- button5 ----
+                button5.setText("5");
+                contentPanel.add(button5, new GridBagConstraints(4, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+
+                //---- button6 ----
+                button6.setText("6");
+                contentPanel.add(button6, new GridBagConstraints(5, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+
+                //---- button7 ----
+                button7.setText("7");
+                contentPanel.add(button7, new GridBagConstraints(6, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+
+                //---- button8 ----
+                button8.setText("8");
+                contentPanel.add(button8, new GridBagConstraints(7, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 0), 0, 0));
+            }
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setBorder(new EmptyBorder(12, 0, 0, 0));
+                buttonBar.setLayout(new GridBagLayout());
+                ((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 80};
+                ((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0};
+
+                //---- okButton ----
+                okButton.setText("OK");
+                buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0), 0, 0));
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JPanel buttonBar;
+    private JButton okButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+    public static void main(String[] args) {
+        new TestPanel();
+    }
+}
